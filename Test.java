54c7fb50f833753b0e133098259d3c5365837299
balaji38738 <bijjapwar123@gmail.com>
2020-03-31 01:01:00 +0530

    First commit

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers: ");
+		int num1 = sc.nextInt(), num2 = sc.nextInt();
+		int result = num1 + num2;
+		System.out.println(result);
+
+	}
+}
